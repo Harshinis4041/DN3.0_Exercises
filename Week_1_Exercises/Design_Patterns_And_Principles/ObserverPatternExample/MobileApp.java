733d673f79c14dcbb0577105d9d1490f0ad616dc
Harshinis4041 <158650670+Harshinis4041@users.noreply.github.com>
2024-07-31 18:26:02 +0530

    Add files via upload

---
+++ Week_1_Exercises/Design_Patterns_And_Principles/ObserverPatternExample/MobileApp.java
@@ -0,0 +1,15 @@
+package ObserverPatternExample;
+
+public class MobileApp implements Observer {
+    private StockMarket stockMarket;
+
+    public MobileApp(StockMarket stockMarket) {
+        this.stockMarket = stockMarket;
+        stockMarket.registerObserver(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Stock price updated in the Mobile App is " + stockMarket.getStockPrice());
+    }
+}
